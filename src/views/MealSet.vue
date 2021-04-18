0e66e6edd74683af486a751ab977ba892d7039ec
gh <2495186452@qq.com>
2021-04-18 16:42:59 +0800

    add command pattern

--- src/views/MealSet.vue
+++ src/views/MealSet.vue
@@ -24,12 +24,49 @@
             </div>
         </el-card>
         <el-card class="box-card">
+        <div slot="header" class="clearfix">
+            <span>Price</span>
+        </div>
+        <div class="text item">
+            €{{mealset.price}}
+        </div>
+    </el-card>
+        <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <span>Price</span>
+                <span>Order</span>
             </div>
-            <div class="text item">
-                €{{mealset.price}}
+            <div class="text item" >
+                <el-row type="flex" justify="start">
+                    {{orders[0].setorder}}
+                </el-row>
+                <el-row type="flex" justify="end" >
+
+                    <el-button @click="addOrder(0)" type="primary" size="mini">+</el-button>
+                    <p>{{orders[0].num}}</p>
+                    <el-button @click="reduceOrder(0)" type="primary" size="mini" >-</el-button>
+                </el-row>
+                <el-row type="flex" justify="start">
+                    {{orders[1].setorder}}
+                </el-row>
+                <el-row type="flex" justify="end" >
+
+                    <el-button @click="addOrder(1)" type="primary" size="mini">+</el-button>
+                    <p>{{orders[1].num}}</p>
+                    <el-button @click="reduceOrder(1)" type="primary" size="mini" >-</el-button>
+                </el-row>
+                <el-row type="flex" justify="start">
+                    {{orders[2].setorder}}
+                </el-row>
+                <el-row type="flex" justify="end" >
+
+                    <el-button @click="addOrder(2)" type="primary" size="mini">+</el-button>
+                    &emsp;
+                    <p>{{orders[2].num}}</p>
+                    &emsp;
+                    <el-button @click="reduceOrder(2)" type="primary" size="mini" >-</el-button>
+                </el-row>
             </div>
+            <el-button @click="submitOrder">Confirm</el-button>
         </el-card>
     </div>
 </template>
@@ -44,7 +81,10 @@
                         staple:'',
                         drink:'',
                         price:0
-                    }
+                    },
+                orders:[{setorder:'MealSetOne',type:1,num:0},
+                    {setorder:'MealSetTwo',type:2,num:0},
+                    {setorder:'MealSetThree',type:3,num:0}]
 
             }
         },
@@ -73,6 +113,34 @@
                 axios.get('http://localhost:8181/mealset/getmealset/3').then(function(resp){
                     _this.mealset = resp.data
                 })
+            },
+            addOrder(i){
+                this.orders[i].num += 1
+            },
+            reduceOrder(i){
+                if(this.orders[i].num-1<0){
+                    this.orders[i].num=0
+                }else {
+                    this.orders[i].num -= 1
+                }
+            },
+            submitOrder(){
+                const _this = this
+                if(_this.orders[0].num+_this.orders[1].num+_this.orders[2].num>0) {
+                    axios.get('http://localhost:8181/mealset/handlemealset/'+_this.orders[0].type+'/'+_this.orders[0].num+'/'+_this.orders[1].type+'/'+_this.orders[1].num+'/'+_this.orders[2].type+'/'+_this.orders[2].num).then(function(resp){
+                        if(resp.data=='success'){
+                            _this.$alert('Order success！', 'message', {
+                                    confirmButtonText: 'OK',
+                                    callback: action => {
+                                        window.location.reload()
+                                    }
+                                })
+                        }
+                    })
+                }else{
+                    alert('orders can not be 0!')
+                }
+                console.log(this.orders)
             }
         }
     }
